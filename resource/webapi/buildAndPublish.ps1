e709ac38c9ad62e783e14b32280bad4558bca0bd
xqfgbc <gb1932002@aliyun.com>
2019-03-03 22:22:51 +0800

    add output for buildAndPublish script

--- resource/webapi/buildAndPublish.ps1
+++ resource/webapi/buildAndPublish.ps1
@@ -3,5 +3,12 @@ $AppName = "webapiapp"
 $ImageName = $HostUrl + "/" +$AppName + ":" + $env:BUILD_NUMBER
 $Dockerfile = "Dockerfile"
 
+Write-Output "image name is: " + $ImageName
+
+Write-Output "begin build image"
 docker build --pull -t $ImageName -f $Dockerfile .
+
+Write-Output "begin push image"
 docker push $ImageName
+
+Write-Output "success build and publish image: " +$ImageName
