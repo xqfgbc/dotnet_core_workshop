7161b75d61497fc7117cce140afdc7da1ce2e876
xqfgbc <gb1932002@aliyun.com>
2019-03-04 23:15:19 +0800

    change jenkinsfile

--- resource/webapi/buildAndPublish.sh
+++ resource/webapi/buildAndPublish.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 HostUrl = "localhost:5002"
 AppName = "webapiapp"
-ImageName = "$HostUrl/$AppName:$BUILD_NUMBER"
+ImageName = "${HostUrl}/${AppName}:${BUILD_NUMBER}"
 Dockerfile = "Dockerfile"
 
-echo "image name is: $ImageName"
+echo "image name is: ${ImageName}"
 
 echo "begin build image"
-docker build --pull -t $ImageName -f $Dockerfile .
+docker build --pull -t ${ImageName} -f ${Dockerfile} .
 
 echo "begin push image"
-docker push $ImageName
+docker push ${ImageName}
 
-echo "success build and publish image: $ImageName"
+echo "success build and publish image: ${ImageName}"
